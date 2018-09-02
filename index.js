62d72ed3cf9e3f660e5bd561e0dd0ae38e9e8209
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-09-02 07:21:58 +0000

    solved problem

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach((item) => {
+    if (!Array.isArray(item)) {
+      sum += item;
+    }
+    else {
+      sum += sumItems(item);
+    }
+  })
+  return sum;
 }
 
 module.exports = sumItems;
